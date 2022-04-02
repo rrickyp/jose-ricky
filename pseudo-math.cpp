b3b19098849d2c5e86c88308af5ffd9e418d73c1
PuTaTus <92213323+PuTaTus@users.noreply.github.com>
2022-04-02 14:16:38 +0800

    Pseudo code for math problem

---
+++ pseudo-math.cpp
@@ -0,0 +1,16 @@
+
+time;
+temp = srand(time);
+temp = time % 10;
+
+if ( 0 <= temp < 3 ){
+    int tmp;
+    int result
+    result = a * b;
+    cout << 3 << "*" << 4 << "is: " << endl;
+    cin << tmp; //user input
+    if(tmp == result){
+        return true;//user can continue.
+    }
+    return false;//user lost the blackjack game.
+}
