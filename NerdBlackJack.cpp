294b2e372bcc92a496a9a42cbe8d331d52401a83
devintern <dev1@weavatools.com>
2022-04-15 17:20:31 +0800

    added the random 1.0

--- NerdBlackJack.cpp
+++ NerdBlackJack.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+
+int random_card(){
+
+
+};
+
+void initial_card(int player[5], int dealer[5]){
+  int i =0;
+  while(i < 2){
+    player[i] = random_card();
+    dealer[i] = random_card();
+    i++;
+  }
+
+}
+
 int main() {
   ifstream fin;
   fin.open("ASCII-NERD.txt");
   string welcome;
   while (getline(fin, welcome)){
-    cout << welcome <<endl;
+    cout << welcome << endl;
+    
+  //
+  initial_card();
+
 
   }
 }
