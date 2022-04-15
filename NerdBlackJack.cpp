e8aa8c97a1a4dbf3273477fc67afd0008792cd70
rrickyp <rickyrp@connect.hku.hk>
2022-04-15 17:16:27 +0800

    Added some new features

--- NerdBlackJack.cpp
+++ NerdBlackJack.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
 using namespace std;
 int main() {
   ifstream fin;
+  system("clear");
   fin.open("ASCII-NERD.txt");
   string welcome;
   while (getline(fin, welcome)){
-    cout << welcome <<endl;
+    cout <<welcome<<endl;
+  }
+  int start = (time(NULL));
+  while (time(NULL) != start+3) {
 
   }
+  cout << "ARE YOU READY TO PLAY??(Y/N)";
+  char ans;
+  cin >> ans;
+  if ((ans != 'Y')||(ans != 'y')) {
+    return 0;
+  }
+  return 0;
 }
