a52c3f0e6c421599a0bf46be2f2a62f4d2358777
rrickyp <rickyrp@connect.hku.hk>
2022-04-15 18:00:15 +0800

    Added the flow of the program

--- NerdBlackJack.cpp
+++ NerdBlackJack.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <cstdlib>
 using namespace std;
 
-int random_card(){
+bool exist(int card, int cards[], int length) { // full of bugs
+  for (int i = 0; i < length; i ++) {
+    if (card == cards[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+int shareCard(int cards[], int length){ //full of bugs
+  srand(time(NULL));
+  int card = rand()%52;
+  while (exist(card, cards, length)) {
+    card = rand()%52;
+  }
+  return card;
+}
 
-
-};
-
-void initial_card(int player[5], int dealer[5]){
+void initial_card(int player[5], int dealer[5], int usedCard[], int &length){ //full of bugs
   int i =0;
   while(i < 2){
-    player[i] = random_card();
-    dealer[i] = random_card();
+    player[i] = shareCard(usedCard, length);
+    usedCard[length] = player[i];
+    length++;
+    dealer[i] = shareCard(usedCard, length);
     i++;
+    usedCard[length] = dealer[i];
+    length++;
   }
 
 }
@@ -32,12 +49,19 @@ int main() {
   while (time(NULL) != start+3) {
 
   }
-  cout << "ARE YOU READY TO PLAY??(Y/N)";
+  cout << "ARE YOU READY TO PLAY?? (Y/N): ";
   char ans;
   cin >> ans;
-  if ((ans != 'Y')||(ans != 'y')) {
+  if ((ans == 'Y')||(ans == 'y')) {
+    int usedCards[15];
+    int PlayerCards[5];
+    int DealerCards[5];
+    int length = 0, playerCards_length = 0;
+    initial_card(PlayerCards, DealerCards, usedCards, length);
+    PrintCard(PlayerCards, playerCards_length);
+    PrintCard(DealerCards, 1); // inside of PrintCard , if length == 1, print the card and the closed card.
+  }
+  else {
     return 0;
   }
-  initial_card();
-  return 0;
 }
